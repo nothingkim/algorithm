e0b5cf4eef5212f2a09b105b99a4fbf38fc6c5d7
nothingkim <rkswkdwhtjs@naver.comrm>
2025-05-21 17:15:23 +0900

    fix

--- swea/22795.py
+++ swea/22795.py
@@ -1,16 +1,10 @@
 T = int(input())
-arr = []
-ans = 0
-sum = 0
-    # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for test_case in range(0, T):
-    arr.append(list(map(int, input().split())))
-    ans = max(arr[test_case])+1
-    for i in range(0, len(arr[test_case])):    
-        sum = sum + int(arr[test_case][i])
+for _ in range(T):
+    arr = list(map(int, input().split()))
+    s = sum(arr)
+    ans = max(arr) + 1
     while True:
-        if (sum + ans)%7 ==0:
+        if (s + ans) % 7 == 0:
             print(ans)
             break
-        else:
-            ans = ans + 1
+        ans += 1
